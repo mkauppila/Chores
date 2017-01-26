398babbdc5ffa830f9479faefc3cc1657d1c8ce9
Markus Kauppila <markus.kauppila@gmail.com>
2017-01-26 20:51:27 +0200

    Add navigators for opening the main view

--- Chores/AppDelegate.swift
+++ Chores/AppDelegate.swift
@@ -8,39 +8,68 @@
 
 import UIKit
 
+// 
+
+class ChoresListNavigator {
+    var rootNavigator: RootNavigator?
+
+    func presentFrom(window: UIWindow) {
+        let vc = ViewController()
+        rootNavigator?.showRoot(viewController: vc, inWindow: window)
+    }
+}
+
+//
+
+class AppDependencies {
+
+    private var choresListNavigator: ChoresListNavigator?
+
+    func installRootViewControllerTo(window: UIWindow) {
+        // use the navigator of the first class
+        // choresListNavigator.presentFromWindow(window)
+        choresListNavigator?.presentFrom(window: window)
+    }
+
+    func createDependencies() {
+        let rootNavigator = RootNavigator()
+
+        choresListNavigator = ChoresListNavigator()
+        choresListNavigator?.rootNavigator = rootNavigator
+    }
+}
+
+// Creates the root navigator for all the other navigators
+class RootNavigator {
+    func showRoot(viewController controller: UIViewController, inWindow window: UIWindow) {
+        let navigationController = UINavigationController(rootViewController: controller)
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+    }
+
+    private func navigationControllerFrom(window: UIWindow) -> UINavigationController? {
+        return window.rootViewController as? UINavigationController
+    }
+}
+
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var dependencies: AppDependencies!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        window = UIWindow()
+
+        if let window = window {
+            dependencies = AppDependencies()
+            dependencies.createDependencies()
+            dependencies.installRootViewControllerTo(window: window)
+        }
+
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
 
